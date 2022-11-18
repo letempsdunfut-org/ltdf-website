d5a49bdfbba6c8cef41247d0e224230d517cc6a2
gregory <gregory.boeroteyssier@maplr.co>
2022-11-18 15:50:59 -0500

    add intl

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,14 @@ import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
 import { Montserrat } from '@next/font/google'
 import { GoogleAnalytics } from "nextjs-google-analytics";
+import { appWithTranslation } from 'next-i18next'
+import React from 'react';
 
 // If loading a variable font, you don't need to specify the font weight
 const montserrat = Montserrat({ subsets: ['latin'] })
 
-
-export default function App({ Component, pageProps }: AppProps) {
-  return (
+const appCore = ({ Component, pageProps }: AppProps) => (
+  <React.Fragment>
     <ChakraProvider theme={extendTheme({
       fonts: {
         heading: montserrat.style.fontFamily,
@@ -19,6 +20,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <GoogleAnalytics strategy="lazyOnload" gaMeasurementId='G-HSVTTS26JN' />
       <Component {...pageProps} />
     </ChakraProvider>
+  </React.Fragment>
+);
 
-  );
-}
+export default appWithTranslation(appCore)
+
