eb954853b91704f8410a6ba11b7ac2a7a9e32262
gregory <gregory.boeroteyssier@maplr.co>
2022-11-20 00:21:27 -0500

    add new layout part 1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,17 +7,19 @@ import { appWithTranslation } from 'next-i18next'
 import React from 'react';
 
 // If loading a variable font, you don't need to specify the font weight
-const montserrat = Montserrat({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ['latin'] });
 
 const appCore = ({ Component, pageProps }: AppProps) => (
-  <React.Fragment>
+  <React.Fragment >
     <ChakraProvider theme={extendTheme({
       fonts: {
         heading: montserrat.style.fontFamily,
         body: montserrat.style.fontFamily,
       }
     })}>
-      <GoogleAnalytics strategy="lazyOnload" gaMeasurementId='G-HSVTTS26JN' />
+      {process.env.NODE_ENV == "development" &&
+        <GoogleAnalytics strategy="lazyOnload" gaMeasurementId='G-HSVTTS26JN' />
+      }
       <Component {...pageProps} />
     </ChakraProvider>
   </React.Fragment>
