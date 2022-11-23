60d060ea23fa2bb7d06de1e545b875f6cd2ef212
gregory <gregory.boeroteyssier@maplr.co>
2022-11-23 11:52:11 -0500

    add new layout part 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,8 @@ import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import { Montserrat } from '@next/font/google'
 import { GoogleAnalytics } from "nextjs-google-analytics";
 import { appWithTranslation } from 'next-i18next'
+import { SessionProvider } from "next-auth/react"
+
 import React from 'react';
 
 // If loading a variable font, you don't need to specify the font weight
@@ -17,10 +19,12 @@ const appCore = ({ Component, pageProps }: AppProps) => (
         body: montserrat.style.fontFamily,
       }
     })}>
-      {process.env.NODE_ENV == "development" &&
+      {process.env.NODE_ENV !== "development" &&
         <GoogleAnalytics strategy="lazyOnload" gaMeasurementId='G-HSVTTS26JN' />
       }
-      <Component {...pageProps} />
+      <SessionProvider>
+        <Component {...pageProps} />
+      </SessionProvider>
     </ChakraProvider>
   </React.Fragment>
 );
