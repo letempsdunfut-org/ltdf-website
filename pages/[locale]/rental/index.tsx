075096e470e6d9bf2571e35121e721b7ebaa5b85
gregory <gregory.boeroteyssier@maplr.co>
2023-02-19 12:24:51 -0500

    fix typo and main image loaging

--- pages/[locale]/rental/index.tsx
+++ pages/[locale]/rental/index.tsx
@@ -39,7 +39,7 @@ export default function Rental() {
                     <Image
                         src={imgCouverture}
                         alt={name}
-                        loading="lazy"
+                        priority={true}
                         fill={true}
                         style={{objectFit: 'cover', objectPosition: 'center', backgroundColor: 'gray'}}
                     />
