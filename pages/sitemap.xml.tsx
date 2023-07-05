a72c31d8a881f298cdccd1c532db6d1c1fefa15e
gregory <gregory@letempsdunfut.ca>
2023-07-05 14:13:10 -0400

    feature/SEO    add basic SEO

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,120 @@
+const Sitemap = () => {
+};
+const createSitemap = (host: string) =>
+    `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
+        xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
+        xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+    <url>
+        <loc>https://${host}.ca/</loc>
+        <lastmod>2023-05-30T17:56:11.165Z</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>https://${host}/auth/login/</loc>
+        <lastmod>2023-05-30T17:56:11.165Z</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>https://${host}/auth/logout/</loc>
+        <lastmod>2023-05-30T17:56:11.165Z</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>https://${host}/booking/</loc>
+        <lastmod>2023-05-30T17:56:11.165Z</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>https://${host}/product/</loc>
+        <lastmod>2023-05-30T17:56:11.165Z</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>https://${host}/professional/</loc>
+        <lastmod>2023-05-30T17:56:11.165Z</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>https://${host}/rental/</loc>
+        <lastmod>2023-05-30T17:56:11.165Z</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>https://${host}/fr/product/</loc>
+        <lastmod>2023-05-30T17:56:11.165Z</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>https://${host}/en/product/</loc>
+        <lastmod>2023-05-30T17:56:11.165Z</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>https://${host}/fr/booking/</loc>
+        <lastmod>2023-05-30T17:56:11.165Z</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>https://${host}/en/booking/</loc>
+        <lastmod>2023-05-30T17:56:11.165Z</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>https://${host}/fr/rental/</loc>
+        <lastmod>2023-05-30T17:56:11.165Z</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>https://${host}/en/rental/</loc>
+        <lastmod>2023-05-30T17:56:11.165Z</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>https://${host}/fr/professional/</loc>
+        <lastmod>2023-05-30T17:56:11.165Z</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>https://${host}/en/professional/</loc>
+        <lastmod>2023-05-30T17:56:11.165Z</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>https://${host}/fr/</loc>
+        <lastmod>2023-05-30T17:56:11.165Z</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+    </url>
+    <url>
+        <loc>https://${host}/en/</loc>
+        <lastmod>2023-05-30T17:56:11.165Z</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+    </url>
+</urlset>`;
+
+export async function getServerSideProps({res, req}: { res: any, req: any }) {
+    res.setHeader("Content-Type", "text/xml");
+    res.write(createSitemap(req.headers.host));
+    res.end();
+    return {props: {results: {}}}
+}
+
+export default Sitemap;
